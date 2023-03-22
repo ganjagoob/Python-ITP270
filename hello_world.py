c51fa6d574097ad1aaa6f0032fb77df98644b100
ganjagoob <justinhuuua@gmail.com>
2023-03-21 21:13:33 -0400

    added Python Basics

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,17 @@
 #!/bin/python3
 
-#print "Hello World!"
-print("Hello World!")
+#1. Create variables for name, age, degree program
+name = ""
+age = 0
+degree_program = ""
+
+#2. Take user input for all three variables
+name = input("Please enter your name ")
+age = int(input("Please enter your age "))
+degree_program = input("Please enter your degree program ")
+
+#3. Calculate remaining age
+remaining_age = (age * 3) % 2
+
+# 4. Print message with string concatenation
+print("Hi my name is "+ name + ". My remaining age is "+ str(remaining_age) + " and I am majoring in " + degree_program + " at NOVA.")
